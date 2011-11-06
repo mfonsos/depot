f42277be002b7ba625fbf714f22f430b743586f7
root <root@localhost.localdomain>
2011-11-06 02:00:50 +0200

    Task D cart creation

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,16 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    if session[:counter].nil?
+       session[:counter] = 0
+       @display_counter == false
+    end
+      @counter = session[:counter] += 1
+      if session[:counter] > 5
+        @display_counter = @counter
+      end
+    
+    
   end
 
 end
